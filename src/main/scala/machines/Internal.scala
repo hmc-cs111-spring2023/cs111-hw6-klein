e577c5abbe25b3e6a1e8b8c52c85bfd191e589d0
Georgia Klein <georgia.r.klein@gmail.com>
2023-02-28 21:40:46 +0000

    Coding compeltion

--- src/main/scala/machines/Internal.scala
+++ src/main/scala/machines/Internal.scala
@@ -19,6 +19,27 @@ extension (exp1: RegularLanguage)
 
     def <+> = Concat(exp1, Star(exp1))
 
-    def apply(n: Int) : RegularLanguage =
-        if (n == 1) then exp1 // Ask about this? I can't get it to work for n == 0, what is the base case?
+    def apply(n: Int) : RegularLanguage = {
+        // Base case of n == 0 is epsilon, which represents an empty word.
+        if (n == 0) then Epsilon
         else exp1 ~ (apply(n - 1))
+    }
+
+    // Contextual parameters: RegularLanguage -> DFA
+    // The alphabet (a Set[Char]) must be given before this method is called.
+    def toDFA(using givenAlph: Set[Char]) = regexToDFA(exp1, givenAlph)
+
+// Implicit conversion: RegularLanguage -> DFA
+given Conversion[RegularLanguage, DFA] = {exp =>
+    regexToDFA(exp, chars(exp))
+}
+
+// Helper function that returns the set of characters that appear in the given regular expression.
+def chars(exp: RegularLanguage) : Set[Char] = {exp match
+    case Empty => Set()
+    case Epsilon => Set()
+    case Character(c) => Set(c)
+    case Union(r1, r2) => chars(r1) ++ chars(r2)
+    case Concat(r1, r2) => chars(r1) ++ chars(r2)
+    case Star(r) => chars(r)
+}
